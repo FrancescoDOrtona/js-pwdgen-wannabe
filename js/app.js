1ea03d2d4af4e9eb1ed9cad6734cd40256800558
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-03 14:56:46 +0200

    Add: html link & script & pseudocode copy in js

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// - Chiedere all'utente il suo nome:
+//     - dichiarare una variabile
+//     - assegnare alla variabile il valore restituito dal prompt
+// - Chiedere all'utente il suo cognome:
+//     - dichiarare una variabile
+//     - assegnare alla variabile il valore restuitutio dal prompt
+// - Chiedere all'utente il suo colore preferito
+//     - dichiarare una variabile
+//     - assegnare alla variabile il valore restuitutio dal prompt
+// - Assegnare un numero random:
+//     - dichiarare una variabile
+//     - specificare la funzione per un numero a caso
+// - Concatenare le 4 variabili per formare una password
+//     - dichiarare una variabile passwordGeneration dove concateno le 4 stringhe
+// - Stampare la password nel dom
+//     - inserire nell'html una tag con classe password
+//     - recuperare con js gli lementi del dom con id = passwordGeneration
+//     - modificare l'innerHTML del tag con il valore della variabile passwordGeneration 
