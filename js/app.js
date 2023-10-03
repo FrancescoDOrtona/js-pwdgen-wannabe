bac0b93319dc8b9c3f5413ea18fb5f60c69ca157
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-03 15:30:32 +0200

    Add: Print password in dom

--- js/app.js
+++ js/app.js
@@ -9,23 +9,25 @@ console.log(userName);
 let userSurname;
 //     - assegnare alla variabile il valore restuitutio dal prompt
 userSurname = prompt ('Cognome');
-console.log(userSurname)
+console.log(userSurname);
 // - Chiedere all'utente il suo colore preferito
 //     - dichiarare una variabile
 let userPrefColor;
 //     - assegnare alla variabile il valore restuitutio dal prompt
-userPrefColor = prompt ('Colore Preferito')
-console.log(userPrefColor)
+userPrefColor = prompt ('Colore Preferito');
+console.log(userPrefColor);
 // - Assegnare un numero random:
 //     - dichiarare una variabile
 //     - specificare la funzione per un numero a caso
 const randomNumber = Math.floor(Math.random() * 100);
-  console.log(randomNumber)
+  console.log(randomNumber);
 // - Concatenare le 4 variabili per formare una password
 //     - dichiarare una variabile passwordGeneration dove concateno le 4 stringhe
 let passwordGeneration = userName + userSurname + userPrefColor + randomNumber
-console.log(passwordGeneration)
+console.log(passwordGeneration);
 // - Stampare la password nel dom
 //     - inserire nell'html una tag con classe password
 //     - recuperare con js gli lementi del dom con id = passwordGeneration
+const pswDomElement = document.getElementById('password');
 //     - modificare l'innerHTML del tag con il valore della variabile passwordGeneration 
+pswDomElement.innerHTML = passwordGeneration;
